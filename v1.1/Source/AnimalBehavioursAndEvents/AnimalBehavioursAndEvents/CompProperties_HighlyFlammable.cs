d1351b77c1e9be954f2975e3ff6b95b44cbb3f9b
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-02-20 11:10:41 +0100

    v2.0

---
+++ v1.1/Source/AnimalBehavioursAndEvents/AnimalBehavioursAndEvents/CompProperties_HighlyFlammable.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+namespace AnimalBehavioursAndEvents
+{
+    public class CompProperties_HighlyFlammable : CompProperties
+    {
+
+       
+
+        public CompProperties_HighlyFlammable()
+        {
+            this.compClass = typeof(CompHighlyFlammable);
+        }
+
+
+    }
+}
---
+++ v1.1/Source/AnimalBehavioursAndEvents/AnimalBehavioursAndEvents/CompProperties_Nocturnal.cs
@@ -0,0 +1,16 @@
+﻿using Verse;
+
+namespace AnimalBehavioursAndEvents
+{
+    public class CompProperties_Nocturnal : CompProperties
+    {
+
+
+        public CompProperties_Nocturnal()
+        {
+            this.compClass = typeof(CompNocturnal);
+        }
+
+
+    }
+}
