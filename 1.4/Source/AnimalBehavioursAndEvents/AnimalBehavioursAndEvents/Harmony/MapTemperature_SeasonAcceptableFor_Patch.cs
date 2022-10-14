5c6862a67e86515f160ecb904dc38e5ed0c8a45e
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-14 13:48:11 +0200

    v4.02

---
+++ 1.4/Source/AnimalBehavioursAndEvents/AnimalBehavioursAndEvents/Harmony/MapTemperature_SeasonAcceptableFor_Patch.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+using Verse.AI;
+
+namespace AnimalBehaviours
+{
+
+    [HarmonyPatch(typeof(MapTemperature))]
+    [HarmonyPatch("SeasonAcceptableFor")]
+    public static class MagicalMenagerie_MapTemperature_SeasonAcceptableFor_Patch
+    {
+        [HarmonyPostfix]
+        public static void AllowAnimalSpawns(ThingDef animalRace, ref bool __result)
+
+        {
+
+           
+
+            if (MagicalMenagerie_Mod.settings.pawnSpawnStates != null && MagicalMenagerie_Mod.settings.pawnSpawnStates.Keys.Contains(animalRace.defName))
+            {
+                if (MagicalMenagerie_Mod.settings.pawnSpawnStates[animalRace.defName])
+                {
+
+                    __result = false;
+                }
+
+            }
+
+
+        }
+    }
+
+
+
+
+
+}
