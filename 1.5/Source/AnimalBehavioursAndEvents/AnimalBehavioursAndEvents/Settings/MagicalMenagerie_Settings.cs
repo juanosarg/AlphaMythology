c38010e75b1ffc31810025ea228def381df0b9ac
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-03-20 19:36:34 +0100

    v5.0

---
+++ 1.5/Source/AnimalBehavioursAndEvents/AnimalBehavioursAndEvents/Settings/MagicalMenagerie_Settings.cs
@@ -0,0 +1,83 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace AnimalBehaviours
+{
+    public class MagicalMenagerie_Settings : ModSettings
+
+    {
+        private static Vector2 scrollPosition = Vector2.zero;
+        public Dictionary<string, bool> pawnSpawnStates = new Dictionary<string, bool>();
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Collections.Look(ref pawnSpawnStates, "pawnSpawnStates", LookMode.Value, LookMode.Value, ref pawnKeys, ref boolValues);
+        }
+        private List<string> pawnKeys;
+        private List<bool> boolValues;
+        private string searchKey;
+
+        public void DoWindowContents(Rect inRect)
+        {
+
+           
+
+            var rect = new Rect(inRect.x, inRect.y, inRect.width, inRect.height);
+            Verse.Text.Anchor = TextAnchor.MiddleLeft;
+            var searchLabel = new Rect(rect.x + 5, rect.y, 60, 24);
+            Widgets.Label(searchLabel, "VEF_AnimalsSearch".Translate());
+            var searchRect = new Rect(searchLabel.xMax + 5, searchLabel.y, 200, 24f);
+            searchKey = Widgets.TextField(searchRect, searchKey);
+            Verse.Text.Anchor = TextAnchor.UpperLeft;
+
+            List<string> keys = pawnSpawnStates.Keys.ToList().OrderBy(x => DefDatabase<ThingDef>.GetNamedSilentFail(x)?.label)?.Where(x => DefDatabase<ThingDef>.GetNamedSilentFail(x)?.label.ToLower().
+            Contains(searchKey.ToLower()) == true)?.ToList();
+            Listing_Standard ls = new Listing_Standard();
+            Rect rectExt = new Rect(inRect.x, searchRect.yMax + 35, inRect.width, inRect.height - 170);
+            Rect rect2 = new Rect(0f, 0f, inRect.width - 30f, keys.Count * 24 + 24);
+            Widgets.BeginScrollView(rectExt, ref scrollPosition, rect2, true);
+
+           
+            if (keys.Count > 0)
+            {
+                for (int num = 0; num < keys.Count; num++)
+                {
+
+                    bool test = pawnSpawnStates[keys[num]];
+                    if (DefDatabase<PawnKindDef>.GetNamedSilentFail(keys[num]) == null)
+                    {
+                        pawnSpawnStates.Remove(keys[num]);
+                    }
+                    else
+                    {
+                        var iconRect = new Rect(0, (num + 1) * 24, 24, 24);
+                        var labelRect = new Rect(30, (num + 1) * 24, inRect.width - 100f, 24);
+                        Widgets.ThingIcon(iconRect, PawnKindDef.Named(keys[num]).race);
+                        Widgets.CheckboxLabeled(labelRect, "MM_DisableAnimal".Translate(PawnKindDef.Named(keys[num]).LabelCap), ref test);
+
+
+                        pawnSpawnStates[keys[num]] = test;
+                    }
+                }
+
+            }
+
+
+
+            ls.End();
+            Widgets.EndScrollView();
+            base.Write();
+
+
+        }
+
+
+    }
+
+   
+
+}
